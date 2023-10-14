9d22c53a7cfd60a08311d09d249c8df3893371e0
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-14 14:02:33 -0400

    Cleaned up and Added Comments

--- src/djs/index.js
+++ src/djs/index.js
@@ -1,14 +1,14 @@
+// Import necessary modules
 require("dotenv").config({ path: "./my.env" });
 const fs = require("fs");
-const {
-  Client,
-  GatewayIntentBits,
-  Collection,
-} = require(`discord.js`);
+const { Client, GatewayIntentBits, Collection } = require(`discord.js`);
 const mongoose = require('mongoose');
+
+// Create a new Discord client
 const client = new Client({
   intents: [
-    GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMembers,
@@ -25,40 +25,47 @@ const client = new Client({
     GatewayIntentBits.GuildMessageTyping,
     GatewayIntentBits.GuildWebhooks,
     GatewayIntentBits.GuildModeration,
-    4 // GuildModeration I think its actually GuildBans
-   ],
-  partials: ['User', 'GuildMember', 'Channel', 'Message', 'Reaction', 'Presence'] 
+    GatewayIntentBits.GuildBans // Ensure this intent is included
+  ],
+  partials: ['User', 'GuildMember', 'Channel', 'Message', 'Reaction', 'Presence']
 });
 
+// Import and set up handlers
 const handleFunctions = require("./client/handlers/handleFunctions");
 const handleEvents = require("./client/handlers/handleEvents");
 const handleCommands = require("./client/handlers/handleCommands");
+
+// Create a collection for databases
 let dbs = new Collection();
+
+// Define folder paths for Discord.js functions, commands, events, and MongoDB configuration
 const djsFunctionFolders = fs.readdirSync("./src/djs/functions");
 const djsCommandFolders = fs.readdirSync("./src/djs/commands");
 const djsEventFiles = fs
   .readdirSync("./src/djs/client/events")
   .filter((file) => file.endsWith(".js"));
-  //const scripts = require("./functions/scripts/scripts.js")
-  const mongoConfig = fs.readdirSync("./src/MongoDB/db/config");
 
-  const {login_mongodb} = require("./functions/ready/login_mongodb.js");
-  const {login_mega} = require("./functions/ready/login_mega.js");
+// Import necessary scripts and configurations
+const mongoConfig = fs.readdirSync("./src/MongoDB/db/config");
+const { login_mongodb } = require("./functions/ready/login_mongodb.js");
+const { login_mega } = require("./functions/ready/login_mega.js");
 
-const { token, MongoDB_Token_DaVinci } = process.env;
+// Get environment variables
+const { token, MongoDB_Token_DaVinci, MEGA_EMAIL, MEGA_PASSWORD } = process.env;
 
-const mega_email = `${process.env.MEGA_EMAIL}`; 
-const mega_password = `${process.env.MEGA_PASSWORD}`; 
+// Define the client's email and password for Mega
+const mega_email = `${MEGA_EMAIL}`;
+const mega_password = `${MEGA_PASSWORD}`;
 
-
-
+// Set up initial properties for the client
 client.connectedToMega;
-
 client.commands = new Collection();
 
+// Handle the "ready" event
 client.on("ready", () => {
   console.log("---------- >> Bot is Online << ----------");
 
+  // Define activity options
   const activities = [
     "with art and knowledge",
     "Leonardo's masterpieces",
@@ -70,60 +77,47 @@ client.on("ready", () => {
     "Studying the Vitruvian Man",
     "Exploring ancient Egypt",
     "Deciphering the Great Pyramids",
-];
-
+  ];
 
   const types = ["PLAYING", "LISTENING", "WATCHING", "STREAMING", "COMPETING"];
 
   const statuses = ["online", "idle", "dnd", "invisible"];
 
+  // Set random activities and presence status
   setInterval(() => {
     const text = activities[Math.floor(Math.random() * activities.length)];
     const type = types[Math.floor(Math.random() * types.length)];
-    // const status = statuses[Math.floor(Math.random() * statuses.length)];
     const status = statuses[1];
-    // client.user.setPresence({activities : [{name: text, type: type}], status: status});
     client.user.setPresence({ activities: [{ name: text }], status: status });
     client.user.setAct;
   }, 60000);
 });
+
+// Export the client for use in other modules
 module.exports = client;
 
-
-
-
-
+// Handle events, functions, and commands
 handleEvents(client, djsEventFiles, 1);
 handleEvents(client, mongoConfig, 2);
 handleFunctions(djsFunctionFolders, "./src/djs/functions");
 
-
 (async () => {
+  try {
+    // Connect to MongoDB
+    await login_mongodb(client, MongoDB_Token_DaVinci);
 
-    try{
-    await login_mongodb(client, MongoDB_Token_DaVinci)
-
-      handleCommands(client, djsCommandFolders, "./src/djs/commands").then( 
-      login_mega(client, mega_email, mega_password).then(async (mega)=>
-      {
-        client.login(token)
-        
+    // Handle commands and login to Mega
+    handleCommands(client, djsCommandFolders, "./src/djs/commands").then(
+      login_mega(client, mega_email, mega_password).then(async (mega) => {
+        // Login to the Discord client
+        client.login(token);
+      }).catch((error) => {
+        console.log(error, "\n\nerror during login_mega in index.js");
+        // Login to the bot client in case of an error
+        client.login(token);
       })
-      .catch((error)=>{
-        console.log(error, "\n\nerror during login_mega in index.js")
-        // login to the bot client
-        client.login(token)
-      })
-      ) // when changing bot tokens, make sure to change client id in handleCommands.js as well otherwise commands will not register to the correct client
-
-      
-      
-      
-    } catch(error){
-      console.log(error, "error during login activities in index.js")
-    }
-    
-  
-})()
-
-
+    );
+  } catch (error) {
+    console.log(error, "error during login activities in index.js");
+  }
+})();
