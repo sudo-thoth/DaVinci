592dac7900e4d3c3b81cbf3f44797e8269525f48
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-15 11:57:39 -0400

    Formatting and new event listening for mega client ready

--- src/djs/functions/ready/login_mega.js
+++ src/djs/functions/ready/login_mega.js
@@ -2,8 +2,8 @@ const { Storage } = require("megajs");
 
 const MAX_RETRIES = 3;
 require("dotenv").config({ path: "../../../../my.env" });
-const mega_email = `${process.env.MEGA_EMAIL}`; 
-const mega_password = `${process.env.MEGA_PASSWORD}`; 
+const mega_email = `${process.env.MEGA_EMAIL}`;
+const mega_password = `${process.env.MEGA_PASSWORD}`;
 const theClient = require("../../index.js");
 
 /**
@@ -26,7 +26,7 @@ async function retryWithMaxAttempts(func, maxAttempts) {
       lastError = error;
       console.error(`Error during attempt ${attempts}:`, error);
       // Delay before the next retry
-      await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time if needed
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Adjust the delay time if needed
     }
   }
 
@@ -42,59 +42,57 @@ async function retryWithMaxAttempts(func, maxAttempts) {
  *
  * @returns {Storage} The logged-in Mega.nz account if successful, or throws an error if login fails.
  */
-async function login_mega(client = theClient, email = mega_email, password = mega_password) {
+async function login_mega(
+  client = theClient,
+  email = mega_email,
+  password = mega_password
+) {
   try {
     // Create a new Mega.nz account object & autologin
-    const mega = new Storage({ email: email, password: password });
-    
-    // Check if the account is already logged in
-    if (!mega?.getAccountInfo()) {
+    const mega = new Storage({ email: email, password: password, autologin: false });
+    client.connectedToMega = false;
+    // listen for "ready" event
+    mega.on("ready", () => {
+      // mega is logged in and ready to be used
+      console.log("✅ Mega Client Ready!");
+      client.connectedToMega = true;
+      client.Mega = mega;
+    });
+
       // Account is not logged in, so log in
       try {
-        console.log("Attempting Mega Client Log In");
+        // console.log("Attempting Mega Client Log In");
         let attempts = 0;
         let lastError = null;
         let delay = 1000; // Initial delay in milliseconds
-    
+
         while (attempts < 3) {
           try {
             attempts++;
             mega.login();
-            console.log("Mega Client Login successful!");
-            client.connectedToMega = true;
-            client.Mega = mega;
             return mega;
           } catch (error) {
             lastError = error;
             console.error(`Error during attempt ${attempts}:`, error);
-          
+
             // Exponential backoff: Increase delay before the next retry
             delay *= 2; // You can adjust the multiplier as needed
-          
+
             console.log(`Retrying in ${delay} milliseconds...`);
-            await new Promise(resolve => setTimeout(resolve, delay));
+            await new Promise((resolve) => setTimeout(resolve, delay));
           }
         }
         throw lastError; // Throw the last error if all attempts failed
-      
       } catch (error) {
         client.connectedToMega = false;
         console.error("Error:", error);
       }
-    } else {
-      console.log("Mega Client currently logged in!");
-      client.connectedToMega = true;
-      client.Mega = mega;
-      return mega;
-    }
   } catch (error) {
     console.error("Error:", error);
     client.connectedToMega = false;
   }
 }
 
-
-
 module.exports = {
-    login_mega
+  login_mega,
 };
