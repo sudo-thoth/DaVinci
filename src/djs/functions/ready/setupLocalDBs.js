d5b3b6c22c48d7269ca643afd64e6b409716bbf8
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-15 22:13:38 -0400

    feat: create setupLocalDBs.js

---
+++ src/djs/functions/ready/setupLocalDBs.js
@@ -0,0 +1,15 @@
+const client = require("../../index.js");
+
+// require the channel, user, and guild schemas
+client.channelsDB = require(`../../../MongoDB/db/schemas/essentialData/schema_channels.js`);
+client.usersDB = require(`../../../MongoDB/db/schemas/essentialData/schema_users.js`);
+client.guildsDB = require(`../../../MongoDB/db/schemas/essentialData/schema_guilds.js`);
+
+// create a local map of guilds with id as key and guild object as value
+client.localGuilds = new Collection();
+
+// create a local log array to store log messages
+client.localLog = [];
+
+// create a local database object to store data for each guild and channel
+client.localDB = {};
