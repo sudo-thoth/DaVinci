ac9d8ecf48ac113ae0f0a85bb08f057608462eb4
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-15 23:13:16 -0400

    update runFunctionEveryXMinutes.js - added max loop option

--- src/djs/functions/scripts/runFunctionEveryXMinutes.js
+++ src/djs/functions/scripts/runFunctionEveryXMinutes.js
@@ -1,17 +1,23 @@
 /**
- * Runs a function every X minutes.
+ * Runs a function every X minutes for a maximum number of loops.
  *
  * @param {number} minutes - The number of minutes to wait between function runs.
  * @param {function} func - The function to run.
+ * @param {number} [maxLoops] - The maximum number of loops to run the function.
  */
-const runFunctionEveryXMinutes = (minutes, func) => {
-  setInterval(async () => {
-    try {
-      await func();
-    } catch (error) {
-      console.log(error, `Error in ${func.name}`);
-    }
-  }, minutes * 60 * 1000);
+const runFunctionEveryXMinutes = (minutes, func, maxLoops = Infinity) => {
+    let loops = 0;
+    const interval = setInterval(async () => {
+        try {
+            await func();
+        } catch (error) {
+            console.log(error, `Error in ${func.name}`);
+        }
+        loops++;
+        if (loops === maxLoops) {
+            clearInterval(interval);
+        }
+    }, minutes * 60 * 1000);
 };
 
 module.exports = runFunctionEveryXMinutes;
