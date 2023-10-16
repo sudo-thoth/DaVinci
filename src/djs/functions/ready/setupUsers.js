291009b7584ef09bfd336321079a79322cd3d7b1
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-15 22:40:12 -0400

    feat: create setupUsers.js

---
+++ src/djs/functions/ready/setupUsers.js
@@ -0,0 +1,22 @@
+const client = require("../../index.js");
+const setupUser = require("./setupUser.js");
+
+/**
+ * Sets up all users the bot is in.
+ *
+ * @param {Object} client - The Discord client object.
+ */
+const setupUsers = async (client) => {
+  // get all the users the bot is in
+  const users = client.users.cache.map((user) => user);
+
+  for (const user of users) {
+    // set up the user object in the database
+    await setupUser(user);
+  }
+  console.log(`User data setup complete for all Users from the client`);
+};
+
+client.setupUsers = setupUsers;
+
+module.exports = setupUsers;
