5928a3edc65588177b5bd8387bf1b0dc01838436
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-15 22:05:01 -0400

    feat: create setupChannels.js

---
+++ src/djs/functions/ready/setupChannels.js
@@ -0,0 +1,26 @@
+const client = require("../../index.js");
+const setupChannel = require("./setupChannel.js");
+
+/**
+ * Sets up all channels the bot is in.
+ *
+ * @param {Object} client - The Discord client object.
+ */
+const setupChannels = async (client) => {
+  // get all the channels the bot is in
+  const channels = client.channels.cache.map((channel) => channel);
+
+  for (const channel of channels) {
+    // create the guild-specific object in the localDB
+    client.localDB[channel.guild.name].channels = {};
+    // create the channel-specific object in the localDB
+    client.localDB[channel.guild.name].channels[channel.name] = {};
+    // set up the channel object in the database
+    await setupChannel(channel);
+  }
+  console.log(`Channel data setup complete for all Channels from the client`);
+};
+
+client.setupChannels = setupChannels;
+
+module.exports = setupChannels;
