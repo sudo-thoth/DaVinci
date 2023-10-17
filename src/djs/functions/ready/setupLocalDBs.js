d72be1adf1fb67d85e3670555dccebb144adad6a
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-17 17:05:59 -0400

    update setupLocalDBs.js - removed some imports

--- src/djs/functions/ready/setupLocalDBs.js
+++ src/djs/functions/ready/setupLocalDBs.js
@@ -4,10 +4,6 @@ const { Collection } = require("discord.js");
 
 
 async function setupLocalDBs() {
-// require the channel, user, and guild schemas
-client.channelsDB = require(`../../../MongoDB/db/schemas/essentialData/schema_channels.js`);
-client.usersDB = require(`../../../MongoDB/db/schemas/essentialData/schema_users.js`);
-client.guildsDB = require(`../../../MongoDB/db/schemas/essentialData/schema_guilds.js`);
 
 // create a local map of guilds with id as key and guild object as value
 client.localGuilds = new Collection();
