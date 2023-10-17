94c9ef16cfcc7f792836a194bda61f2040fba58d
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-17 17:03:09 -0400

    update commandCenter.js - added kick

--- src/djs/functions/prefixCommandHandling/commandCenter.js
+++ src/djs/functions/prefixCommandHandling/commandCenter.js
@@ -1,8 +1,10 @@
 const setCommandStyle = require("../commands/Administration/setCommandStyle.js");
+const kick = require("../commands/Moderation/kick.js");
 
 
 
 
 module.exports = {
     setCommandStyle,
+    kick,
 }
