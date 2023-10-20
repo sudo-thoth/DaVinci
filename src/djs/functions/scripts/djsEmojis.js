dc3e3dcb4355c03e0ad6d130e121bd7f22fd36d3
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-10-20 17:22:13 -0400

    feat: create djsEmojis.js - Add emojis for loading, voting, and check badges.

---
+++ src/djs/functions/scripts/djsEmojis.js
@@ -0,0 +1,20 @@
+
+// loading emojis
+const loading_dotted = "<a:loading:1152790181120835635>"; // 3 dots animation
+const loading_spinwheel = "<a:loading:1094842418521722941>"; // spinwheel animation
+const loading_google = "<a:Discord_loading:1094515757074874428>"; // google animation
+const loading_circle = "<a:verify:1100873948041855017>"; // circle animation
+
+// voting emojis
+const voting_yes = "<:voteyes:1107544992857194536>"; // green glowing check
+const voting_no = "<:no:1142561965538025472>"; // red glowing cross
+
+// check badges
+const check_badge_green = "<a:finished:1094842420480462909>"; // green check badge animation
+const check_badge_red = "<a:red_check:1147032392616398848>" // red check badge animation
+const check_badge_black = "<a:Check:1147032041397964810>" // black check badge animation
+const check_badge_white = "<a:check:1147032076936302592>" // white check badge animation
+const check_badge_gold = "<a:check2:1147031982329581668>" // gold check badge animation
+
+// other emojis
+const check_verified = "<a:verifiedcheck:1147032451982561392>"; // verified green circle check animation
