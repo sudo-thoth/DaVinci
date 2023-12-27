3a7754ef1335a0ba456a4a82459b60f0d705feff
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2023-12-27 17:38:29 -0500

    Add permaBan and ban commands

--- src/djs/functions/prefixCommandHandling/commandCenter.js
+++ src/djs/functions/prefixCommandHandling/commandCenter.js
@@ -1,7 +1,11 @@
 const setCommandStyle = require("../commands/Administration/setCommandStyle.js");
 const kick = require("../commands/Moderation/kick.js");
+const permaBan = require("../commands/Moderation/permaBan.js");
+const ban = require("../commands/Moderation/ban.js");
 
 module.exports = {
   setCommandStyle,
   kick,
+  permaBan,
+  ban,
 };
