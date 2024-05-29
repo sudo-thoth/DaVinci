f29140bfb5a9a244388f955cf5ba26ee831339f5
sudo-thoth <109883548+sudo-thoth@users.noreply.github.com>
2024-05-28 20:22:43 -0400

    Perfect runFunctionEveryXMinutes and allow variables to be passed in

--- src/djs/functions/scripts/runFunctionEveryXMinutes.js
+++ src/djs/functions/scripts/runFunctionEveryXMinutes.js
@@ -3,13 +3,17 @@
  *
  * @param {number} minutes - The number of minutes to wait between function runs.
  * @param {function} func - The function to run.
- * @param {number} [maxLoops] - The maximum number of loops to run the function.
+ * @param {object} [vars={}] - An object containing the variables to pass to the function.
+ * @param {number} [maxLoops=Infinity] - The maximum number of loops to run the function.
  */
-const runFunctionEveryXMinutes = (minutes, func, maxLoops = Infinity) => {
+const runFunctionEveryXMinutes = (minutes, func, vars = {}, maxLoops = Infinity) => {
     let loops = 0;
     const interval = setInterval(async () => {
         try {
-            await func();
+            // Extract the possible variable values from the vars object
+            const varValues = Object.values(vars);
+            // Call the function with the extracted values; if vars is empty, this will be equivalent to calling the function with no arguments
+            await func(...varValues);
         } catch (error) {
             console.log(error, `Error in ${func.name}`);
         }
